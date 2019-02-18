b9e49fdf0d9aacfab608a1357612f2b82a0695e7
crossaaron <atcbcrossx@gmail.com>
2019-02-18 11:17:46 -0800

    added initial structure build

---
+++ Components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return(
+            <div id='app-container'>
+                <h1>New App</h1>
+                <p>application init</p>
+            </div>
+        )
+
+
+    }
+}
